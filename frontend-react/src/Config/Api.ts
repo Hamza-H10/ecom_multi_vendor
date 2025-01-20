64b2b7b5b80341a809190f68ba96312e248456d9
hamza-H10 <hamza.official010@gmail.com>
2025-01-20 14:26:03 +0530

    production url added

--- frontend-react/src/Config/Api.ts
+++ frontend-react/src/Config/Api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export const API_URL = "http://localhost:5454";
-export const DEPLOYED_URL = "https://zosh-bazzar-backend.onrender.com"
+// export const DEPLOYED_URL = "https://zosh-bazzar-backend.onrender.com"
+export const DEPLOYED_URL = "https://ecommultivendor-production.up.railway.app/"
 // change api
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  // baseURL: API_URL, 
+  baseURL: DEPLOYED_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
